546bede3ceb728b84a38ce468dbfb2590e80b492
Tuan Le <lehtuan776@gmail.com>
2022-03-14 19:05:02 -0400

    added subtraction

--- lab6.java
+++ lab6.java
@@ -5,13 +5,28 @@ public class lab6 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input a number to add: ");
-        int intputOne = scanner.nextInt();
+        System.out.println("Would you like to add or subtract numbers?");
+        String addOrSubtract = scanner.next();
 
-        System.out.println("Input another number to add: ");
-        int intputTwo = scanner.nextInt();
+        if (addOrSubtract == "add" || addOrSubtract == "Add"){
+            System.out.println("Input a number to add: ");
+            int intputOne = scanner.nextInt();
 
-        System.out.println("Your sum is: " + " " + intputOne + intputTwo);
+            System.out.println("Input another number to add: ");
+            int intputTwo = scanner.nextInt();
+
+            System.out.println("Your sum is: " + " " + (intputOne + intputTwo));
+        }
+        else if(addOrSubtract == "subtract" || addOrSubtract == "Subtract"){
+            System.out.println("Input a number to subtract: ");
+            int intputOne = scanner.nextInt();
+
+            System.out.println("Input another number to subtract: ");
+            int intputTwo = scanner.nextInt();
+
+            System.out.println("Your difference is: " + " " + (intputOne - intputTwo));
+        }
+
     }
 
 }
